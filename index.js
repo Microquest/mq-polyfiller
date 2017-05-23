fbb6dd360447832c134cbc3ba5bc8c04f9e0d933
Steven Myers <steven.myers@microquest.ca>
2017-05-23 13:44:34 -0600

    misspelled polyfill

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-require('./dist/shim/pollyfiller.min.js');
+require('./dist/shim/polyfiller.min.js');
 require('./dist/shim/form-shim-extend.min.js');
 require('./dist/shim/shim.css');
 require('./dist/shim/forms-ext.css');
